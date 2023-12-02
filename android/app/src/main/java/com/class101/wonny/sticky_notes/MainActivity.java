53f9ef1b57b1fc930e4f8bcb1928d456f503624e
wonny <onlyone728@gmail.com>
2023-12-02 19:33:40 +0900

    sticky_notes 진행

---
+++ android/app/src/main/java/com/class101/wonny/sticky_notes/MainActivity.java
@@ -0,0 +1,6 @@
+package com.class101.wonny.sticky_notes;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/data/note_manager.dart
@@ -0,0 +1,25 @@
+import 'note.dart';
+
+class NoteManager {
+  List<Note> _notes = [];
+
+  void addNote(Note note) {
+    _notes.add(note);
+  }
+
+  void deleteNote(int index) {
+    _notes.removeAt(index);
+  }
+
+  Note getNote(int index) {
+    return _notes[index];
+  }
+
+  List<Note> listNote() {
+    return _notes;
+  }
+
+  void updateNote(int index, Note note) {
+    _notes[index] = note;
+  }
+}
